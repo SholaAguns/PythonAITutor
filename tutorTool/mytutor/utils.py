729d2ac6f0114a7432d372a1e587b4fbdeb7b757
Shola Aguns <sholzyaguns@live.com>
2024-06-14 16:30:31 +0100

    Chat Gpt incorporated

--- tutorTool/mytutor/utils.py
+++ tutorTool/mytutor/utils.py
@@ -1,21 +1,9 @@
 import os
 from openai import OpenAI
 
-os.environ["OPENAI_API_KEY"] = "sk-proj-dL6yIdQ2XJWq9xqo6RYzT3BlbkFJfMBKbzGhTptZcVssxNt4"
-#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "sk-proj-dL6yIdQ2XJWq9xqo6RYzT3BlbkFJfMBKbzGhTptZcVssxNt4")
+os.environ["OPENAI_API_KEY"] = "<your key>"
 
-# def get_gpt_response(prompt):
-#     response = openai.ChatCompletion.create(
-#         model="gpt-3.5-turbo",  # or "gpt-4" if you have access
-#         #model="gpt-4",  # or "gpt-4" if you have access
-#         messages=[
-#             {"role": "system", "content": "You are a tutor."},
-#             {"role": "user", "content": prompt}
-#         ],
-#         max_tokens=200,
-#         temperature=0.7,
-#     )
-#     return response.choices[0].message['content'].strip()
+
 
 def get_gpt_response(prompt):
     client = OpenAI(
